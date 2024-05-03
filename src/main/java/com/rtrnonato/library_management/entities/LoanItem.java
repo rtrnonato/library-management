fe63d7f5e17d306acc4d3f6fe3600b060ca928c2
Artur <rtrnonato@gmail.com>
2024-05-02 22:58:54 -0300

    Commenting LoanItem

--- src/main/java/com/rtrnonato/library_management/entities/LoanItem.java
+++ src/main/java/com/rtrnonato/library_management/entities/LoanItem.java
@@ -17,31 +17,58 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+/**
+ * Representa um item associado a um empréstimo no sistema de gerenciamento de biblioteca.
+ */
 @Entity
 @Table(name = "tb_loan_item")
 public class LoanItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	// Identificador composto deste item de empréstimo.
 	@EmbeddedId
 	private LoanItemPK id = new LoanItemPK();
 	
+	// Empréstimo associado a este item de empréstimo.
 	@ManyToOne
 	@JoinColumn(name = "loan_item_id")
 	private Loan loan;
 	
+	// Conjunto de itens de empréstimo associados a este item.
 	@OneToMany(fetch = FetchType.EAGER)
     private Set<LoanItem> items = new HashSet<>();
 	
+	// Data de retorno esperada do livro associado a este item.
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "GMT")
 	private LocalDate expectedReturn;
 	
+	// Data de retorno real do livro associado a este item.
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "GMT")
 	private LocalDate actualReturn;
 	
+	/**
+     * Construtor padrão.
+     */
 	public LoanItem() {
 	}
 	
+	/**
+     * Construtor com argumentos.
+     * 
+     * @param book O livro associado a este item de empréstimo.
+     * @param loan O empréstimo associado a este item de empréstimo.
+     * @param expectedReturn A data de retorno esperada do livro.
+     * @param actualReturn A data de retorno real do livro.
+     * @throws IllegalArgumentException Se algum dos parâmetros for nulo.
+     */
 	public LoanItem(Book book, Loan loan, LocalDate expectedReturn, LocalDate actualReturn) {
+		if (book == null) {
+	        throw new IllegalArgumentException("Book cannot be null");
+	    }
+	    if (loan == null) {
+	        throw new IllegalArgumentException("Loan cannot be null");
+	    }
+	    
 		id.setBook(book);
 		id.setLoan(loan);
 		this.expectedReturn = expectedReturn;
@@ -56,7 +83,6 @@ public class LoanItem implements Serializable {
 		id.setBook(book);
 	}
 	
-	 @JsonIgnore
 	public Loan getLoan() {
 		return id.getLoan();
 	}
@@ -65,7 +91,6 @@ public class LoanItem implements Serializable {
 		id.setLoan(loan);
 	}
 	
-	 @JsonIgnore
 	public LoanItemPK getId() {
 		return id;
 	}
@@ -106,5 +131,4 @@ public class LoanItem implements Serializable {
 		LoanItem other = (LoanItem) obj;
 		return Objects.equals(id, other.id);
 	}
-	
 }
